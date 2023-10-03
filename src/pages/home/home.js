ea89aab9e32ac4bdb90d222e862a6fee7c250a61
Isaac Flores <jordyngv14@outlook.com>
2023-10-03 10:21:28 -0600

    Update home.js

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
+//import axios from "axios";
 import Cookies from 'universal-cookie'
 
 
@@ -8,8 +8,6 @@ const cookies = new Cookies()
 const meicimg = "logo.png";
 const alegaimg = "logo_alega.png";
 const URI = "https://fwmback-production.up.railway.app/asepress";
-
-
 
 function Home() {
   const [ agente, setAgente ] = useState(cookies.get('info'))
@@ -23,6 +21,7 @@ function Home() {
   }
 
   const [ reportes, setReportes ] = useState([])
+  /*
   useEffect(() => {
     getReportes()
   }, [])
@@ -32,7 +31,7 @@ function Home() {
     const report = res.data
     setReportes(report)
   }
-
+*/
   return (
     <>
       <nav className="navbar bg-body-white fixed-top position-relative shadow">
